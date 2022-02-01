69955ba09c73c3fd613f6a024ac93cf020af3d1c
Ekachcka <2331286yan@gmail.com>
2022-02-01 11:17:39 +0200

    Add pr7

--- pr7.js
+++ pr7.js
@@ -1,10 +1,31 @@
-const a=0.735, b=0.745,E=0.0001
-let  xcp=(a+b)/2, x1=a+1/2*(b-a), x2=a+1/2*(b-a)
+const a = 0.735, b = 0.745, E = 0.0001
+let x1 , x2,n=0,k
 
-
-function fun(x){
-    return (x*x/2-Math.sin(x));
+function fib(n) {
+    var a = 1,
+        b = 1;
+    for (var i = 3; i <= n; i++) {
+        var c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
-console.log("f1="+fun(x1));
-console.log("f2="+fun(x2));
+function fun(x) {
+    return (x * x / 2 - Math.sin(x));
+}
+
+for (let index = false; index === false; n++) {
+    if ((b-a)/fib(n)<=E) {
+        index=true
+    }
+}
+n--;
+k=n-2;
+for (;n>=0 ; n--,k--) {
+    console.log('x1='+(x1=a+fib(k)/fib(n)*(b-a)))
+    console.log('x2='+(x2=a+fib(k+1)/fib(n)*(b-a)))
+    console.log("f1=" + fun(x1));
+    console.log("f2=" + fun(x2));
+}
